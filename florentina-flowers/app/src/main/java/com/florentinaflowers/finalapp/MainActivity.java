3040f57aea4c4c4d05721da2988034f1512b48c6
mariusurlan1980-arch <mariusurlan1980@gmail.com>
2026-09-25 18:19:05 +0300

    Add final app MainActivity package

---
+++ florentina-flowers/app/src/main/java/com/florentinaflowers/finalapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.florentinaflowers.finalapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    public class AppBridge {
+        @JavascriptInterface
+        public void openCheckout(String url) {
+            runOnUiThread(() -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url))));
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+         s.setAllowFileAccess(true);
+        s.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.addJavascriptInterface(new AppBridge(), "Android");
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html?build=11");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
