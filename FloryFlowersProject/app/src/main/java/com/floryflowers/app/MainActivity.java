7ad20fdb5d91f791d8ce46114648804c873b3d04
mariusurlan1980-arch <mariusurlan1980@gmail.com>
2026-09-26 23:04:10 +0300

    Add FloryFlowersProject/app/src/main/java/com/floryflowers/app/MainActivity.java

---
+++ FloryFlowersProject/app/src/main/java/com/floryflowers/app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.floryflowers.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
